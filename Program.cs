0c167042b0ee9c35dcc48c8315e8d577d7070246
Michael Wang <michael.wang@myob.com>
2020-08-24 16:11:51 +1000

    Completed kata

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int number = 1; number < 100; number++)
+            {
+                if(number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            }
         }
     }
 }
